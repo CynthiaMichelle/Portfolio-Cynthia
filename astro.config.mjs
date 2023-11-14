9fe2dc3047109e095331ebba26e974b2c8ee88ad
Cynthia <cynthiacruz1007@gmail.com>
2023-11-14 17:22:03 +0100

    quitando github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-    site: 'https://CynthiaMichelle.github.io',
-    base: '/Portfolio-Cynthia',
-  })
+// https://astro.build/config
+export default defineConfig({});
