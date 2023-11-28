b8adc7c44d8caa220016a46958227cdf021ce900
Cynthia <cynthiacruz1007@gmail.com>
2023-11-28 17:53:18 +0100

    pruea

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/configgg//
+// https://astro.build/config//
 export default defineConfig({
     site: 'https://CynthiaMichelle.github.io',
     base: '/Portfolio-Cynthia',
